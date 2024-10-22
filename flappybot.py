a55e0b86fb41280c3ccd7a4812e1cd50a85156a9
Marek Guráň <skemgi2@gmail.com>
2024-10-22 23:33:01 +0200

    Chnaged layout and new links

--- flappybot.py
+++ flappybot.py
@@ -6,22 +6,24 @@ from telegram.request import HTTPXRequest
 # Define the bot token
 TOKEN = 'TOKEN'
 
-# Define the buttons
+# Define the buttons in the specified layout with additional rows
 buttons = [
-    [InlineKeyboardButton("Flappy PEPE", callback_data='flappy_pepe')],
-    [InlineKeyboardButton("Pepecoin.org", url="https://pepecoin.org/")],
-    [InlineKeyboardButton("Pepe Explorer", url="https://pepeexplorer.com/")],
-    [InlineKeyboardButton("Pepelum", url="http://pepelum.site/")],
-    [InlineKeyboardButton("Pepeblocks", url="https://pepeblocks.com/")],
-    [InlineKeyboardButton("Pepeblocks Faucet", url="https://faucet.pepeblocks.com/")],
-    [InlineKeyboardButton("Ravener Faucet", url="https://pepe.ravener.is-a.dev/")],
-    [InlineKeyboardButton("Stakecube Faucet", url="https://stakecube.net/app/community")]
+    [InlineKeyboardButton("Flappy PEPE", callback_data='flappy_pepe'), InlineKeyboardButton("Pepelum", url="http://pepelum.site/")],
+    [InlineKeyboardButton("Pepeblocks Faucet", url="https://faucet.pepeblocks.com/"), InlineKeyboardButton("Ravener Faucet", url="https://pepe.ravener.is-a.dev/")],
+    [InlineKeyboardButton("Stakecube Faucet", url="https://stakecube.net/app/community"), InlineKeyboardButton("Pepecoin.org", url="https://pepecoin.org/")],
+    [InlineKeyboardButton("Pepe Explorer", url="https://pepeexplorer.com/"), InlineKeyboardButton("Pepeblocks", url="https://pepeblocks.com/")],
+    [InlineKeyboardButton("Pepecoin Telegram Group", url="https://t.me/PepecoinGroup")],
+    [InlineKeyboardButton("Discord", url="https://discord.gg/pepecoin"), InlineKeyboardButton("Reddit", url="https://reddit.com/r/Pepecoin")]
 ]
+
+# The rest of the code remains the same
+
+# The rest of the code remains the same
 
 # Start command handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = InlineKeyboardMarkup(buttons)
-    await update.message.reply_text('Choose an option:', reply_markup=keyboard)
+    await update.message.reply_text('Choose an option:',reply_markup=keyboard)
 
 # Button press handler
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
